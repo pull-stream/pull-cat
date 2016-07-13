aa80fccc40aa4a454e1c99ad92db645fb917a136
Michael Williams <michael.williams@enspiral.com>
2016-07-13 12:32:51 +1200

    standard style

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 var noop = function () {}
 
-function all(ary, abort, cb) {
+function all (ary, abort, cb) {
   var n = ary.length
-  if(!n) return cb(abort)
+  if (!n) return cb(abort)
   ary.forEach(function (f) {
-    if(f) f(abort, next)
+    if (f) f(abort, next)
     else next()
   })
 
-  function next() {
-    if(--n) return
+  function next () {
+    if (--n) return
     cb(abort)
   }
-  if(!n) next()
+  if (!n) next()
 }
 
 module.exports = function pullCat (streams) {
   return function (abort, cb) {
     ;(function next () {
-      if(abort)
+      if (abort) {
         all(streams, abort, cb)
-      else if(!streams.length)
+      } else if (!streams.length) {
         cb(true)
-      else if(!streams[0])
-        streams.shift(), next()
-      else
+      } else if (!streams[0]) {
+        streams.shift()
+        next()
+      } else {
         streams[0](null, function (err, data) {
-          if(err) {
+          if (err) {
             streams.shift()
-            if(err !== true) {
+            if (err !== true) {
               abort = err
-              //abort all streams
-              while(streams.length)
+              // abort all streams
+              while (streams.length) {
                 (streams.shift() || noop)(err, noop)
+              }
               cb(err)
             }
             next()
-          }
-          else
-            cb(null, data)
+          } else cb(null, data)
         })
+      }
     })()
   }
 }
